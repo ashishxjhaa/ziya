e08df25aeaadf6284a7ae9445fd227ab11676601
Ashish Jha <ashishxyzjha@gmail.com>
2025-09-09 22:07:47 +0530

    Theme Animation Done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable}  bg-neutral-50 dark:bg-neutral-950 overflow-x-hidden antialiased`}
       >
         <SplashLayout>
           <AnimatedLayout>
--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -8,16 +8,23 @@ export function ThemeButton() {
     const { theme, setTheme } = useTheme();
 
     const toggleTheme = () => {
+        if (!document.startViewTransition) {
+            setTheme(theme === "dark" ? "light" : "dark");
+            return;
+        }
+
+        document.startViewTransition(() => {
+            setTheme(theme === "dark" ? "light" : "dark");
+        });
+
         const audio = new Audio("/lightdarkmode.mp3");
         audio.volume = 0.2;
         audio.play();
-
-        setTheme(theme === "dark" ? "light" : "dark");
     };
 
 
     return (
-        <button onClick={toggleTheme} className="px-2.5 py-2 cursor-pointer rounded-md dark:hover:bg-black hover:bg-[#F2F2F2] relative z-10" aria-label={`Switch to ${theme === "dark" ? "light" : "dark"} theme`}>
+        <button onClick={toggleTheme} className="px-2.5 py-2 cursor-pointer rounded-md dark:hover:bg-black hover:bg-white relative z-10" aria-label={`Switch to ${theme === "dark" ? "light" : "dark"} theme`}>
             {theme === "light" ? (
                 <Moon className="h-5 w-5 scale-100 rotate-0 transition-all dark:scale-0 dark:rotate-90" />
             ) : (
