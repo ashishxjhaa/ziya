2fe8188b14f0f88ff047493db851cd7a4cb69bd4
Ashish Jha <ashishxyzjha@gmail.com>
2025-09-11 13:32:53 +0530

    Issue Fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="overflow-x-hidden min-h-screen">
       <Navbar />
       <Hero />
       <Hero1 />
